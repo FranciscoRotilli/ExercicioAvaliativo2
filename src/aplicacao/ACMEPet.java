2f4f9ba6c0a556dca1570b8209a9b35bb2cf25f5
Francisco Coimbra Rotilli <francisco.r@edu.pucrs.br>
2025-05-08 16:50:03 -0300

    implementando metodo para ler arquivos csv

--- src/aplicacao/ACMEPet.java
+++ src/aplicacao/ACMEPet.java
@@ -1,5 +1,15 @@
 package aplicacao;
 import dados.Bicharada;
+import dados.Pet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ACMEPet {
 	private Bicharada bicharada;
@@ -10,6 +20,25 @@ public class ACMEPet {
 
 	public void executar() {
 
+	}
+
+	private ArrayList<ArrayList<String>> lerDados(String arquivo) {
+		Path path = Paths.get(arquivo);
+		ArrayList<ArrayList<String>> dados = new ArrayList<>();
+		try (BufferedReader reader = Files.newBufferedReader(path, Charset.defaultCharset())) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] valores = line.split(",");
+
+				ArrayList<String> linha = new ArrayList<>(Arrays.asList(valores));
+
+				dados.add(linha);
+			}
+		}
+		catch (IOException e) {
+			System.err.format("Erro de E/S: %s%n", e);
+		}
+		return dados;
 	}
 
 }
